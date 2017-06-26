0563c2d146895e444943f3f824560fb09d7dc133
Evgeniy Chernov <evgeniy.chernov@gmail.com>
2017-06-26 17:31:10 +0400

    Add files via upload

---
+++ cycles.js
@@ -0,0 +1,22 @@
+for (var i = 2; i <= 10; i +=2) {
+  alert(i);
+
+} 
+
+var i = 0;
+
+while (i < 3) {
+
+  alert( "номер " + i + "!" );
+
+  i++;
+
+}
+
+while (true) {
+
+  var number = prompt("¬ведите число больше 100","");
+
+  if ((number > 100) || (number == null)) break;
+
+}
