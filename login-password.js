16b032ccdc5c80881017d5c495cd8fecda4fd6d1
Evgeniy Chernov <evgeniy.chernov@gmail.com>
2017-06-26 15:01:09 +0400

    Add files via upload

---
+++ login-password.js
@@ -0,0 +1,16 @@
+var login = prompt("Кто пришел?","");
+
+if (login == null) alert("Вход отменен");
+
+else if (login != "Админ") alert("Я вас не знаю");
+
+else {
+  var password = prompt("Пароль?","");
+
+  if (password == null) alert("Вход отменен");
+
+  else if (password == "Черный Властелин") alert("Добро пожаловать");
+
+  else alert("Пароль неверен");
+
+}  
