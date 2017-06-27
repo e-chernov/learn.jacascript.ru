a531538971b7890b3f92c168e08ca525c8ccf8eb
Evgeniy Chernov <evgeniy.chernov@gmail.com>
2017-06-27 21:29:16 +0400

    Add files via upload

---
+++ functions1-2-3.js
@@ -0,0 +1,29 @@
+function checkAge(age) {
+
+  return (age > 18) ? true : confirm('Родители разрешили?');
+
+}
+
+
+function checkAge(age) {
+
+  return (age > 18) || confirm('Родители разрешили?');
+
+}
+
+
+function min(a,b) {
+
+  if (a < b) return a;
+
+  else if (a > b) return b;
+
+  else return "Неизвестно";
+
+}
+
+alert(min(2,5));
+
+alert(min(4,3));
+
+alert(min(6,6));
