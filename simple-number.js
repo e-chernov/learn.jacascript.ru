fed120781932b72f75e0cea885b6074db7549804
Evgeniy Chernov <evgeniy.chernov@gmail.com>
2017-06-27 19:40:46 +0400

    Add files via upload

---
+++ simple-number.js
@@ -0,0 +1,33 @@
+var min = 2, max = 10;
+
+for (var i = min; i <= max; i++) {
+
+  var simple = false;
+
+  if (i == 2) simple = true;
+
+  else {
+
+    var j = 2;
+
+    while (j < i) {
+
+      if ((i % j) == 0) {
+
+        simple = false;
+
+        break;
+
+      }
+
+      simple = true;
+
+      j++;
+
+    }
+
+  }
+
+  if (simple) alert(i);
+
+}
