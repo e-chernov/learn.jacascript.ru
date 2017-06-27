7113e46302e0c064ce064314cca0e4ec41c3741f
Evgeniy Chernov <evgeniy.chernov@gmail.com>
2017-06-27 21:40:17 +0400

    Add files via upload

---
+++ function-pow.js
@@ -0,0 +1,19 @@
+var x = prompt("Возведем число:", "");
+
+var n = prompt("В степень:");
+
+function pow(x,n) {
+
+  var result = 1;
+
+  for (var i = 1; i <= n; i++) {
+
+    result = result * x;
+
+  }
+
+  return result;
+
+}
+
+alert(pow(x,n));
