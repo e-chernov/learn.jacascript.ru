f61b1b1502b640b39e50c45853fbbb18ee9a5851
Evgeniy Chernov <evgeniy.chernov@gmail.com>
2017-07-06 21:06:25 +0400

    Add files via upload

---
+++ binet.js
@@ -0,0 +1,35 @@
+function fibBinet(n) {
+  var phi = (1 + Math.sqrt(5)) / 2;
+
+  return Math.round( Math.pow(phi, n) / Math.sqrt(5))
+
+}
+
+alert( fibBinet(77) );
+
+
+function fib(n) {
+
+  var a = 1,
+
+    b = 0,
+
+    x;
+
+  for (i = 0; i < n; i++) {
+
+    x = a + b;
+
+    a = b
+
+    b = x;
+
+  }
+
+  return b;
+
+}
+
+
+
+alert( fib(77) );
