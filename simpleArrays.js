6b2309bfb94d744db26bb81bc6a8c1bdb0cdefa0
Evgeniy Chernov <evgeniy.chernov@gmail.com>
2017-07-09 23:50:48 +0400

    Add files via upload

---
+++ simpleArrays.js
@@ -0,0 +1,8 @@
+goods[goods.length-1]
+goods.push["Компьютер"]
+
+var styles = ["Джаз", "Блюз"];
+styles.push("Рок-н-Ролл");
+styles[styles.length - 2] = "Классика";
+alert( styles.shift() );
+styles.unshift("Рэп", "Регги");
