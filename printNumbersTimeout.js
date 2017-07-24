743adcbc7ebd3d606d0bd213ae01b92fa73476fb
Evgeniy Chernov <evgeniy.chernov@gmail.com>
2017-07-24 23:08:24 +0400

    Add files via upload

---
+++ printNumbersTimeout.js
@@ -0,0 +1,10 @@
+function printNumbersInterval() {
+   var i = 1;
+   var timerId = setTimeout(function run() {
+      console.log(i);
+      if (i < 20) setTimeout(run, 100);
+      i++;
+   }, 100);
+};
+
+printNumbersInterval();
